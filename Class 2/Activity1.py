ad6ea51c01e5762be1c56da36613ca03473f1fa4
AayushDeus123 <aayushdeus312@gmail.com>
2024-12-04 14:56:05 +0530

    Add files via upload

---
+++ Class 2/Activity1.py
@@ -0,0 +1,19 @@
+#Print is a pre-definined function
+print("Hello World")
+print(123+456-789)
+
+#Variables
+a='Aayush'
+x=12
+y=34
+print(a, "is a person")
+print(x*y)
+print("x*y =", x*y)
+print(x,'*', y, '=', x*y)
+
+#Keywords
+import keyword
+print("Python keywords are... \n" )
+print(keyword.kwlist)
+count=len(keyword.kwlist)
+print(count)
---
+++ Class 2/Homework(Class2).py
@@ -0,0 +1,21 @@
+print('Friends of Raj and their respective birthdays:\n')
+#Variables for names
+a = 'Aayush'
+b = 'Benjamin'
+c = 'Christofer'
+d = 'Daniel'
+e = 'Ethan'
+
+#Variables for birthdays
+ab = '3 December 2010' 
+bb = '26 July 2009'
+cb = '16 June 2010'
+db = '11 April 2011'
+eb = '7 January 2009'
+
+#Code
+print(ab,'-',a)
+print(bb,'-',b)
+print(cb,'-',c)
+print(db,'-',d)
+print(eb,'-',e)
